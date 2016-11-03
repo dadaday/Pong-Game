062793694b8db4d5dc1733e66f7cc04527a616d1
dadaday <aaurub@gmail.com>
2016-11-03 18:09:42 +0600

    copied contents of practice 1 to practice 2

---
+++ Practice 2/Assets/BallScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour {
+
+	public float forceScale = 100.0f;
+
+	private Rigidbody ball;
+
+	// Use this for initialization
+	void Start () {
+		ball = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontalAxis = Input.GetAxis ("Horizontal");
+		float verticalAxis = Input.GetAxis ("Vertical");
+
+		Vector3 force = new Vector3 (horizontalAxis, 0, verticalAxis);
+		force *= forceScale;	
+
+		ball.AddForce (force);
+	}
+}
