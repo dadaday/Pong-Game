710e7e3bf3a1bada1647ff46cdb1e4ed637583b8
dadaday <aaurub@gmail.com>
2016-11-04 01:45:02 +0600

    copied from practice 2 to practice 3, added sounds

---
+++ Practice 3/Assets/BallScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour {
+
+	public float forceScale = 500.0f;
+	public float InitialAngle = 20.0f;
+
+	public AudioClip WallSound;
+	public AudioClip PaddleSound;
+
+	private AudioSource aSource;
+	private Rigidbody ball;
+
+	// Use this for initialization
+	void Start () {
+		aSource = GetComponent<AudioSource> ();
+
+		float randFloat = Random.value * 25.0f; //get random value of range [0, 25] and add it to the InitialAngle
+		
+		ball = GetComponent<Rigidbody> ();
+
+		Vector3 force = Quaternion.Euler(0.0f, InitialAngle + randFloat, 0.0f) * Vector3.forward *  forceScale;
+
+		ball.AddForce (force);
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		GameObject gObj = collision.gameObject;
+
+		if (gObj.CompareTag ("Wall")) {
+			aSource.PlayOneShot (WallSound);
+		} else if (gObj.CompareTag ("Paddle")) {
+			aSource.PlayOneShot (PaddleSound);
+		}
+		
+	}
+}
---
+++ Practice 3/Assets/GameController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+	public GameObject ballTemplate;
+
+	public Text firstPlayerScore;
+	public Text secondPlayerScore;
+
+	private AudioSource aSource;
+
+	private int firstPlayerScoreCounter;
+	private int secondPlayerScoreCounter;
+
+	void Start() {
+		aSource = GetComponent<AudioSource> ();
+	}
+
+	void OnTriggerExit(Collider other) {
+		GameObject gameObj = other.gameObject;
+
+		if (gameObj.CompareTag ("Ball")) {
+
+			if (gameObj.transform.position.z < transform.position.z) {
+				++firstPlayerScoreCounter;
+				firstPlayerScore.text = firstPlayerScoreCounter.ToString ();
+			} else {
+				++secondPlayerScoreCounter;
+				secondPlayerScore.text = secondPlayerScoreCounter.ToString ();							
+			}
+
+			aSource.Play ();
+
+			Destroy (gameObj);
+			Instantiate (ballTemplate);
+		}
+		
+	}
+}
