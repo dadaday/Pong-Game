1191eee13ab398da44e50b37c5ea9c42ca6f1347
dadaday <aaurub@gmail.com>
2016-11-21 05:58:42 +0600

    Added Start menu

---
+++ Task 2/Assets/Menu/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public Canvas QuitMenu;
+	public Canvas StartMenu;
+
+	void Start() {
+		QuitMenu = QuitMenu.GetComponent<Canvas> ();
+		StartMenu = StartMenu.GetComponent<Canvas> ();
+	}
+
+	public void LoadScene(string name) {
+		SceneManager.LoadScene (name);
+	}
+
+	public void ExitPress() {
+		QuitMenu.enabled = true;
+		StartMenu.enabled = false;
+	}
+
+	public void ChooseNo() {
+		QuitMenu.enabled = false;
+		StartMenu.enabled = true;
+	}
+
+	public void ChooseYes() {
+		Application.Quit ();
+	}
+}
